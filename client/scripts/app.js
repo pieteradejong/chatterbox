ef0aa6aaaa0ba5fc3165c46cd4c598b37355cc83
Hack Reactor Students <pieter.a.dejong@gmail.com>
2013-10-07 23:03:27 -0700

    modified layout, implementing send message functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,59 +1,48 @@
-// setInterval(function() {
-//   $.ajax({
-//   // always use this url
-//   url: 'https://api.parse.com/1/classes/chatterbox',
-//   type: 'GET',
-// //  data: JSON.stringify(message),
-//   contentType: 'application/json',
-//   success: function (data) {
-//     process(data.results);
-//   },
-//   error: function (data) {
-//     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//     console.error('chatterbox: Failed to send message');
-//   }
-// });},1000);
+var latestMessageTimestamp = 0;
+var username = window.location.search.split('=')[1];
 
-// var process = function(data) {
-//   displayMessages(data);
-//   setInterval(function(data) {
-//     displayMessages(data);
-//   },1000);
-// };
-
-var displayMessages = function (data, latestMessageTimestamp) {
-  var latestMessageTimestamp = latestMessageTimestamp || new Date();
+var displayMessages = function (data,hasRun) {
   _(data).each(function(message) {
-    var msgTimestamp = message.createdAt.getTime();
-    if (msgTimestamp < latestMessageTimestamp) {
-      // continue;
-    } else {
-      var msgClean;
-      console.log(data);
-      var thisDate = message.createdAt.toString().slice(11,16);
+    var msgClean,thisDate,messageString, msgTimestamp;
+    msgTimestamp = (new Date(message.createdAt)).getTime();
+    if (msgTimestamp > latestMessageTimestamp) {
+    // newer messages get displayed:
+      thisDate = message.createdAt.toString().slice(11,16);
       if (message.text !== undefined) {
         msgClean = message.text.replace("<script>", "");
         msgClean = msgClean.replace("</script>", "");
+        // msgClean.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g,'&gt;'').replace(/"/g, '&quot;').replace(/'/g, '&#039;');
       }
 
-      var messageString = "<div>" + message.username +
-      " in room " + message.roomname + ": " +
-      msgClean + "(" + thisDate + ")" + "</div>";
-      $("#main").append(messageString);
+      messageString = "<li class='message'>";
+      messageString += "<span class='username'>" + message.username + "</span>";
+      messageString += "<span class='room'> (in room " + message.roomname + ")</span>";
+      messageString += "<span class='content'> " +msgClean + "</span>";
+      messageString += "<span class='date'>" + thisDate + "</span>";
+      messageString += "</li>";
 
-      latestMessageTimestamp = message.createdAt.getTime();
+      if (hasRun === false) {
+        $('#chatbox').append(messageString);
+      } else {
+        $("#chatbox").prepend(messageString);
+      }
     }
-    // var latestMessage=
   });
+  hasRun = true;
+  latestMessageTimestamp = (new Date(data[0].createdAt)).getTime();
+  // console.log("latestMT="+latestMessageTimestamp);
 };
 
-var updateMessages = function() {
+var updateMessages = function(hasRun) {
   $.ajax({
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
     contentType: 'application/json',
+    data: {order: '-createdAt',
+      limit: 25},
     success: function (data) {
-      displayMessages(data.results);
+//      console.log(data.results.length);
+      displayMessages(data.results,hasRun);
     },
     error: function (data) {
       console.error('chatterbox: Failed to send message');
@@ -61,13 +50,34 @@ var updateMessages = function() {
   });
 };
 
+var sendMessage = function(username, msg) {
+  console.log("sending message");
+  var message = {
+    username: username,
+    text: msg,
+    roomname: 'thisroom'
+  };
+  $.ajax({
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'POST',
+  data: JSON.stringify(msg),
+  contentType: 'application/json',
+  success: function (response) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (response) {
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message');
+  }
+});
+};
+
 
 var run = function() {
-  updateMessages();
-  displayMessages();
+  var hasRun = false;
+  updateMessages(hasRun);
   setInterval(function() {
-    updateMessages();
-    displayMessages();
+    updateMessages(hasRun);
   }, 1000);
 };
 
